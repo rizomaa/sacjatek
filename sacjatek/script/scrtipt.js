81cb8430c039350d310f6002932e3cf5577fb58c
kkerdyanova <kristina.kerdyanova@gmail.com>
2023-04-18 18:54:53 +0500

    update: change section uniqueness

--- sacjatek/script/scrtipt.js
+++ sacjatek/script/scrtipt.js
@@ -14,3 +14,24 @@ function videoOpen () {
 playButton.addEventListener("click", function () {
   videoOpen();
 });
+
+
+
+// section uniqueness
+
+const buttonUniqueness = document.querySelector("#button-uniqueness");
+const blockUniquenessInactive = document.querySelectorAll(".block-uniqueness-content_inactive");
+
+buttonUniqueness.addEventListener("click", function() {
+  for(let i=0; i<blockUniquenessInactive.length; i++) {
+    if(blockUniquenessInactive[i].classList.contains("block-uniqueness-content_inactive")) {
+      blockUniquenessInactive[i].classList.remove("block-uniqueness-content_inactive");
+      buttonUniqueness.classList.add("block-uniqueness-button-active");
+      buttonUniqueness.innerHTML = "Паказаць меньш";
+    } else {
+      blockUniquenessInactive[i].classList.add("block-uniqueness-content_inactive");
+      buttonUniqueness.classList.remove("block-uniqueness-button-active");
+      buttonUniqueness.innerHTML = "Паказаць больш";
+    }
+  }
+})
