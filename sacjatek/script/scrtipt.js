360bbfdff6cdfa5a1d9975d4fa95372efbe3b3a0
kkerdyanova <kristina.kerdyanova@gmail.com>
2023-04-18 21:57:37 +0500

    update: change section obtaining

--- sacjatek/script/scrtipt.js
+++ sacjatek/script/scrtipt.js
@@ -20,18 +20,38 @@ playButton.addEventListener("click", function () {
 // section uniqueness
 
 const buttonUniqueness = document.querySelector("#button-uniqueness");
-const blockUniquenessInactive = document.querySelectorAll(".block-uniqueness-content_inactive");
+const ItemUniquenessInactive = document.querySelectorAll(".item-uniqueness_inactive");
 
 buttonUniqueness.addEventListener("click", function() {
-  for(let i=0; i<blockUniquenessInactive.length; i++) {
-    if(blockUniquenessInactive[i].classList.contains("block-uniqueness-content_inactive")) {
-      blockUniquenessInactive[i].classList.remove("block-uniqueness-content_inactive");
-      buttonUniqueness.classList.add("block-uniqueness-button-active");
+  for(let i=0; i<ItemUniquenessInactive.length; i++) {
+    if(ItemUniquenessInactive[i].classList.contains("item-uniqueness_inactive")) {
+      ItemUniquenessInactive[i].classList.remove("item-uniqueness_inactive");
+      buttonUniqueness.classList.add("button-uniqueness-active");
       buttonUniqueness.innerHTML = "Паказаць меньш";
     } else {
-      blockUniquenessInactive[i].classList.add("block-uniqueness-content_inactive");
-      buttonUniqueness.classList.remove("block-uniqueness-button-active");
+      ItemUniquenessInactive[i].classList.add("item-uniqueness_inactive");
+      buttonUniqueness.classList.remove("button-uniqueness-active");
       buttonUniqueness.innerHTML = "Паказаць больш";
     }
   }
-})
+});
+
+
+// section obtaining
+
+const buttonObtaining = document.querySelector("#button-obtaining");
+const ItemObtainingInactive = document.querySelectorAll(".item-obtaining_inactive");
+
+buttonObtaining.addEventListener("click", function() {
+  for(let i=0; i<ItemObtainingInactive.length; i++) {
+    if(ItemObtainingInactive[i].classList.contains("item-obtaining_inactive")) {
+      ItemObtainingInactive[i].classList.remove("item-obtaining_inactive");
+      buttonObtaining.classList.add("button-obtaining-active");
+      buttonObtaining.innerHTML = "Паказаць меньш";
+    } else {
+      ItemObtainingInactive[i].classList.add("item-obtaining_inactive");
+      buttonObtaining.classList.remove("button-obtaining-active");
+      buttonObtaining.innerHTML = "Паказаць больш";
+    }
+  }
+});
