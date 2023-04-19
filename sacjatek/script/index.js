d88eb61ecf32daaca500781e54f6699a9b69a8a6
kkerdyanova <kristina.kerdyanova@gmail.com>
2023-04-19 15:42:32 +0500

    feat: add slider

--- sacjatek/script/index.js
+++ sacjatek/script/index.js
@@ -1,3 +1,5 @@
+import images from "./images.js";
+
 // section content 
 
 const playButton = document.querySelector(".list-content_button");
@@ -54,4 +56,56 @@ buttonObtaining.addEventListener("click", function() {
       buttonObtaining.innerHTML = "Паказаць больш";
     }
   }
+});
+
+// burger menu 
+
+
+
+// slider
+
+const sliderButtonPrev = document.querySelector(".slider-button-prev");
+const sliderButtonNext = document.querySelector(".slider-button-next");
+const sliderImage = document.querySelector(".slider-image");
+const sliderTitle = document.querySelector(".slider-title");
+const sliderText = document.querySelector(".slider-text");
+let num = 0;
+
+
+function getImageSlider () {
+  sliderImage.src = images[num].img;
+  sliderImage.alt = images[num].name;
+  sliderTitle.innerHTML = `${images[num].name}, ${images[num].years}`;
+  sliderText.innerHTML = images[num].description;
+  console.log(num);
+}
+
+getImageSlider ();
+
+
+function getSlideNext () {
+  num = num+1;
+  if(num > images.length-1) {
+    num = 0;
+  }
+
+  getImageSlider (num);
+}
+
+function getSlidePrev () {
+  num = num-1;
+  if(num < 0) {
+    num = images.length-1;
+  }
+  getImageSlider (num);
+}
+
+sliderButtonPrev.addEventListener("click", function () {
+  getSlidePrev ();
+  console.log("click")
+});
+
+sliderButtonNext.addEventListener("click", function () {
+  getSlideNext ();
+  console.log("click")
 });
