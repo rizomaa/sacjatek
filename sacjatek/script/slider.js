7d1fdf185d3a7fd0dc334bb9af9914b5e2b53cb8
kkerdyanova <kristina.kerdyanova@gmail.com>
2023-05-10 19:03:35 +0500

    feat: add video

---
+++ sacjatek/script/slider.js
@@ -0,0 +1,46 @@
+import images from "./images.js";
+
+// slider
+
+const sliderButtonPrev = document.querySelector(".slider-button-prev");
+const sliderButtonNext = document.querySelector(".slider-button-next");
+const sliderImage = document.querySelector(".slider-image");
+const sliderTitle = document.querySelector(".slider-title");
+const sliderText = document.querySelector(".slider-text");
+let num = 0;
+
+function getImageSlider() {
+  sliderImage.src = images[num].img;
+  sliderImage.alt = images[num].name;
+  sliderTitle.innerHTML = `${images[num].name}, ${images[num].years}`;
+  sliderText.innerHTML = images[num].description;
+}
+
+getImageSlider();
+
+function getSlideNext() {
+  num = num + 1;
+  if (num > images.length - 1) {
+    num = 0;
+  }
+
+  getImageSlider(num);
+}
+
+function getSlidePrev() {
+  num = num - 1;
+  if (num < 0) {
+    num = images.length - 1;
+  }
+  getImageSlider(num);
+}
+
+sliderButtonPrev.addEventListener("click", function () {
+  getSlidePrev();
+  console.log("click");
+});
+
+sliderButtonNext.addEventListener("click", function () {
+  getSlideNext();
+  console.log("click");
+});
